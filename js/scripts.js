9d44a5bfd8ab9eceec039dabb9a3de63508798b7
Aidan Portwood <aidanportwood0@gmail.com>
2024-03-04 13:29:17 -0800

    fix javascript

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  document.getElementById("submit").onclick = function() {
+    event.preventDefault()
+    favFood = document.querySelector('input[name="favFood"]:checked').value
+    favColor = document.querySelector('input[name="favColor"]:checked').value
+    if (favFood === "pizza" && favColor === "red" || favFood === "sandwich" && favColor === "black" || favFood ==="burger" && favColor === "black" || favFood === "pizza" && favColor === "black" || favFood === "sandwich" && favColor === "green" || favFood === "burger" && favColor === "green" || favFood === "pizza" && favColor === "green" || favFood === "sandwich" && favColor === "red" || favFood === "burger" && favColor === "red" ) {
+      document.querySelector("p").innerText = "you are a " + favFood + " " + favColor
+    } else {
+      document.querySelector("p").innerText = "make sure to select one for both"
+    }
+  };
+};
