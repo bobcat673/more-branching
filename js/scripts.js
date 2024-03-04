62b8f5104afcbce74667e7609ab634cee45fafb0
Aidan Portwood <aidanportwood0@gmail.com>
2024-03-04 13:41:41 -0800

    add README

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ window.onload = function() {
     favFood = document.querySelector('input[name="favFood"]:checked').value
     favColor = document.querySelector('input[name="favColor"]:checked').value
     if (favFood === "pizza" && favColor === "red" || favFood === "sandwich" && favColor === "black" || favFood ==="burger" && favColor === "black" || favFood === "pizza" && favColor === "black" || favFood === "sandwich" && favColor === "green" || favFood === "burger" && favColor === "green" || favFood === "pizza" && favColor === "green" || favFood === "sandwich" && favColor === "red" || favFood === "burger" && favColor === "red" ) {
-      document.querySelector("p").innerText = "you are a " + favFood + " " + favColor
+      document.querySelector("p").innerText = "you are a " + favColor + " " + favFood
     } else {
       document.querySelector("p").innerText = "make sure to select one for both"
     }
